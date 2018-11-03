0ed281cbe58da6f6f0cec009ee1e15588b51a416
SH42913 <Shoot13@list.ru>
2018-11-03 14:52:22 +0500

    Init commit

---
+++ GSW2RH/Startup.cs
@@ -0,0 +1,15 @@
+﻿[assembly: Rage.Attributes.Plugin(
+    "GSW2",
+    Description = "This is the GunShot Wound 2. It will bring most realistic damage system to GTAV.",
+    Author = "SH42913")]
+
+namespace GSW2RH
+{
+    public static class Startup
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+}
